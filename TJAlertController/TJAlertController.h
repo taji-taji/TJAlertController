04d58466d0305df8d342d8c852668aa7b354ccc9
Yutaka Tajika <y.tajika529@gmail.com>
2016-11-09 08:46:04 +0900

    Initial Commit

---
+++ TJAlertController/TJAlertController.h
@@ -0,0 +1,19 @@
+//
+//  TJAlertController.h
+//  TJAlertController
+//
+//  Created by tajika on 2016/11/09.
+//  Copyright © 2016年 Tajika. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TJAlertController.
+FOUNDATION_EXPORT double TJAlertControllerVersionNumber;
+
+//! Project version string for TJAlertController.
+FOUNDATION_EXPORT const unsigned char TJAlertControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TJAlertController/PublicHeader.h>
+
+
